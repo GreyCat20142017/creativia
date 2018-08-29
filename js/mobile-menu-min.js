b543714ed5a805c634adeec38dd902e73048be56
GreyCat20142017 <nrz3siaatg81@mail.ru>
2018-08-29 14:00:51 +0300

    Updates

---
+++ js/mobile-menu-min.js
@@ -0,0 +1,1 @@
+"use strict";!function(){var e=document.querySelector(".header__navigation"),n=document.getElementById("id-toggle");null!==e&&null!==n&&(e.classList.contains("header__navigation--nojs")&&e.classList.remove("header__navigation--nojs"),n.addEventListener("click",function(){e.classList.toggle("header__navigation--closed"),e.classList.toggle("header__navigation--opened")}))}();
